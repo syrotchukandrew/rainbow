afd7e551eef1744ac4492e1e8b09e0eff5171fa6
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-02-18 23:32:44 +0200

    Added twitter and googlePlus buttons

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -39,9 +39,45 @@ class AppExtension extends \Twig_Extension
         return array(
             new \Twig_SimpleFunction('dots3', array($this, 'dots3'), array('is_safe' => array('html'))),
             new \Twig_SimpleFunction('facebookButton', array($this, 'getFacebookLikeButton'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('twitterButton', array($this, 'getTwitterButton'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('googlePlusButton', array($this, 'getGooglePlusButton'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('socialButtons', array($this, 'getSocialButtons'), array('is_safe' => array('html'))),
             new \Twig_SimpleFunction('locales', array($this, 'getLocales')),
 
         );
+    }
+
+    public function getSocialButtons($parameters = array())
+    {
+        // no parameters were defined, keeps default values
+        if (!array_key_exists('facebook', $parameters)){
+            $render_parameters['facebook'] = array();
+            // parameters are defined, overrides default values
+        }else if(is_array($parameters['facebook'])){
+            $render_parameters['facebook'] = $parameters['facebook'];
+            // the button is not displayed
+        }else{
+            $render_parameters['facebook'] = false;
+        }
+
+        if (!array_key_exists('twitter', $parameters)){
+            $render_parameters['twitter'] = array();
+        }else if(is_array($parameters['twitter'])){
+            $render_parameters['twitter'] = $parameters['twitter'];
+        }else{
+            $render_parameters['twitter'] = false;
+        }
+
+        if (!array_key_exists('googleplus', $parameters)){
+            $render_parameters['googleplus'] = array();
+        }else if(is_array($parameters['googleplus'])){
+            $render_parameters['googleplus'] = $parameters['googleplus'];
+        }else{
+            $render_parameters['googleplus'] = false;
+        }
+
+        // get the helper service and display the template
+        return $this->container->get('app.socialBarHelper')->socialButtons($render_parameters);
     }
 
     // https://developers.facebook.com/docs/reference/plugins/like/
@@ -58,6 +94,34 @@ class AppExtension extends \Twig_Extension
             );
 
         return $this->container->get('app.socialBarHelper')->facebookButton($parameters);
+    }
+
+    public function getTwitterButton($parameters = array())
+    {
+        $parameters = $parameters + array(
+                'url' => 'http://symfony.com',
+                'locale' => 'en',
+                'message' => 'I want to share that page with you',
+                'text' => 'Tweet',
+                'via' => 'The Acme team',
+                'tag' => 'ttot',
+            );
+
+
+        return $this->container->get('app.socialBarHelper')->twitterButton($parameters);
+    }
+
+    public function getGooglePlusButton($parameters = array())
+    {
+        $parameters = $parameters + array(
+                'url' => 'http://symfony.com',
+                'locale' => 'en',
+                'size' => 'medium',
+                'annotation' => 'bubble',
+                'width' => '300',
+            );
+
+        return $this->container->get('app.socialBarHelper')->googlePlusButton($parameters);
     }
 
     public function getLocales()
