0a32f54a980ad881768a260083f039b1326c19c8
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-02-25 23:32:05 +0200

    added new action

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,9 +2,16 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Estate;
+use AppBundle\Entity\File;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Form\EstateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Gedmo\Uploadable\FileInfo\FileInfoArray;
+
 
 /**
  * @Route("/admin")
@@ -18,7 +25,56 @@ class AdminController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('AppBundle::admin/index.html.twig', array(
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
+        ));
+    }
+
+    /**
+     * @Route("/newestate", name="admin_new_estate")
+     * @Method({"GET", "POST"})
+     */
+    public function newEstateAction(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $estate = $this->getDoctrine()->getRepository('AppBundle:Estate')->find(1);
+        //$estate = new Estate();
+        //$this->denyAccessUnlessGranted('create', $estate);
+        $form = $this->createForm(EstateType::class, $estate)
+            ->add('saveAndCreateNew', SubmitType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //$estate->setDistrict('Казбет');
+            $uploadableManager = $this->container->get('stof_doctrine_extensions.uploadable.manager');
+            $uploadableLisener = $this->container->get('stof_doctrine_extensions.listener.uploadable');
+
+            /*foreach ($estate->getFiles() as $file) {
+                $image = new File();
+                $image = $uploadableLisener->addEntityFileInfo($image, new FileInfoArray($file));
+               // $entityManager->persist($image);
+                $uploadableManager->markEntityToUpload($file, $image);
+            }*/
+
+            if (isset($_FILES['images']) && is_array($_FILES['images'])) {
+                foreach ($_FILES['images'] as $fileInfo) {
+                    $image = new File();
+                    $uploadableManager->markEntityToUpload($image, new FileInfoArray($fileInfo));
+                    $entityManager->persist($image);
+                }
+            }
+
+            /*foreach ($request->files as $file) {
+                $um->markEntityToUpload($estate, $file);
+            }*/
+            $entityManager->persist($estate);
+            $entityManager->flush();
+            $nextAction = $form->get('saveAndCreateNew')->isClicked()
+                ? 'admin_new_estate'
+                : 'admin_index';
+            return $this->redirectToRoute($nextAction);
+        }
+        return $this->render('@App/admin/newestate.html.twig', array(
+            'estate' => $estate,
+            'form' => $form->createView(),
         ));
     }
 }
---
+++ src/AppBundle/Form/EstateType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use AppBundle\Form\FileType as TypeFile;
+
+
+
+
+class EstateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, array(
+                'attr' => array('autofocus' => true,),
+                'label' => 'Title',
+            ))
+            ->add('description', TextareaType::class, array(
+                'attr' => [
+                    'placeholder' => 'Add description',
+                    'class' => 'form-control',
+                    'rows' => 5
+                ]
+            ))
+           /* ->add('files', CollectionType::class, array(
+                'entry_type'   => new TypeFile(),
+                'allow_add'    => true,
+            ))*/
+            ->add('images', 'file', array(
+                'data_class' => 'AppBundle\Entity\File',
+                'required' => false,
+                'attr' => array(
+                    'accept' => 'image/*',
+                    'multiple' => 'multiple',
+                ),
+                'mapped' => false,
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Estate',
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_estate_type';
+    }
+}
