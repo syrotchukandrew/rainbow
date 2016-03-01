76b4efe26fc5e05947f664ec4f8bdc6e60b0e99b
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-03-02 00:19:03 +0200

    brushed estates

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -24,7 +24,6 @@ class AdminController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('AppBundle::admin/index.html.twig', array(
             'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
         ));
@@ -41,76 +40,24 @@ class AdminController extends Controller
         $pagination = $paginator->paginate(
             $estates,
             $request->query->getInt('page', 1),
-            20
+            10
         );
-
         return $this->render('@App/admin/estates.html.twig', array('pagination' => $pagination));
     }
 
     /**
      * @Route("/estate/show/{slug}", name="admin_estate_show")
      * @Method("GET")
+     * @ParamConverter("estate", options={"mapping": {"slug": "slug"}})
      */
-    public function estateShowAction(Estate $estate, $slug)
+    public function estateShowAction(Estate $estate, Request $request)
     {
-        $estate = $this->getDoctrine()->getRepository('AppBundle:Estate')->findOneBy(array('slug' => $slug));
         $deleteForm = $this->createDeleteForm($estate);
         return $this->render('@App/admin/show_estate.html.twig', array(
             'estate'        => $estate,
             'delete_form' => $deleteForm->createView(),
         ));
     }
-
-    /**
-     * @Route("/estate/edit/{slug}", name="admin_estate_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function estateEditAction(Estate $estate, Request $request)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $editForm = $this->createForm(EstateType::class, $estate);
-        $deleteForm = $this->createDeleteForm($estate);
-        $editForm->handleRequest($request);
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $estate = $this->get('app.file_manager')->fileManager($estate);
-            $entityManager->persist($estate);
-            $entityManager->flush();
-            return $this->redirectToRoute('admin_estate_show', array('slug' => $estate->getSlug()));
-        }
-        return $this->render('@App/admin/edit_estate.html.twig', array(
-            'estate'        => $estate,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * @Route("/estate/delete/{slug}", name="admin_estate_delete")
-     * @Method("DELETE")
-     */
-    public function estateDeleteAction(Request $request, Estate $estate)
-    {
-        $form = $this->createDeleteForm($estate);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-
-            $entityManager->remove($estate);
-            $entityManager->flush();
-        }
-        return $this->redirectToRoute('admin_index');
-    }
-
-    private function createDeleteForm(Estate $estate)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('admin_estate_delete', array('slug' => $estate->getSlug())))
-            ->setMethod('DELETE')
-            ->getForm()
-            ;
-    }
-
 
     /**
      * @Route("/estate/new", name="admin_estate_new")
@@ -136,5 +83,57 @@ class AdminController extends Controller
             'estate' => $estate,
             'form' => $form->createView(),
         ));
+    }
+
+    /**
+     * @Route("/estate/edit/{slug}", name="admin_estate_edit")
+     * @Method({"GET", "POST"})
+     * @ParamConverter("estate", options={"mapping": {"slug": "slug"}})
+     */
+    public function estateEditAction(Estate $estate, Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $editForm = $this->createForm(EstateType::class, $estate);
+        $deleteForm = $this->createDeleteForm($estate);
+        $editForm->handleRequest($request);
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $estate = $this->get('app.file_manager')->fileManager($estate);
+            $entityManager->persist($estate);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin_estate_show', array('slug' => $estate->getSlug()));
+        }
+        return $this->render('@App/admin/edit_estate.html.twig', array(
+            'estate'        => $estate,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/estate/delete/{slug}", name="admin_estate_delete")
+     * @Method("DELETE")
+     * @ParamConverter("estate", options={"mapping": {"slug": "slug"}})
+
+     */
+    public function estateDeleteAction(Request $request, Estate $estate)
+    {
+        $form = $this->createDeleteForm($estate);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->remove($estate);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('admin_index');
+    }
+
+    private function createDeleteForm(Estate $estate)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_estate_delete', array('slug' => $estate->getSlug())))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
     }
 }
