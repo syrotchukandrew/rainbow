7b5d49184e6321d152738469081e582688e0b551
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-03-22 15:41:48 +0200

    added tests for sitecontroller

--- src/AppBundle/Tests/Controller/SiteControlerTest.php
+++ src/AppBundle/Tests/Controller/SiteControlerTest.php
@@ -4,7 +4,8 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use AppBundle\Entity\Estate;
-use AppBundle\Entity\Category;
+use AppBundle\Entity\MenuItem;
+use AppBundle\Entity\User;
 
 /**
  * Execute the application tests using this command (requires PHPUnit to be installed):
@@ -44,17 +45,74 @@ class SiteControllerTest extends WebTestCase
     public function testShowCategory()
     {
         $client = static::createClient();
-        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
-        $slug = $em
-            ->getRepository('AppBundle:Category')
-            ->findOneBy([])->getTitle();
-        $crawler = $client->request('GET', "en/show_category/{$slug}");
+        $categories = $client->getContainer()->get('app.final_category_finder')->findFinalCategories();
+        $crawler = $client->request('GET', "en/show_category/{$categories[0]->getTitle()}");
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertEquals(
-            1,
-            $crawler->filter('h3')->count()
+            5,
+            $crawler->filter('h2')->count()
         );
     }
 
+    public function testShowMenuItem()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', "/en/menu_item");
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $menuItems = $em
+            ->getRepository('AppBundle:MenuItem')
+            ->findAll();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(
+            count($menuItems),
+            $crawler->filter('li')->count()
+        );
+    }
 
+    public function testShowDescriptionMenuItem()
+    {
+        $client = static::createClient();
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $menuItems = $em
+            ->getRepository('AppBundle:MenuItem')
+            ->findAll();
+
+        $crawler = $client->request('GET', "/en/description_menu/{$menuItems[0]->getId()}}");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertCount(
+            1,
+            $crawler->filter('h1')
+        );
+    }
+
+    public function testPdfEstate()
+    {
+        $client = static::createClient();
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $estates = $em
+            ->getRepository('AppBundle:Estate')
+            ->findAll();
+        $client->request('GET', "/en/pdf/{$estates[0]->getSlug()}");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testAddEstateToFavorites()
+    {
+        $client = static::createClient();
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $slug = $em
+            ->getRepository('AppBundle:Estate')
+            ->findOneBy([])->getSlug();
+        $users = $em
+            ->getRepository('AppBundle:User')
+            ->findByRole('ROLE_MANAGER');
+        $user = $users[0];
+        $countBefore = count($user->getEstates());
+        $crawler = $client->request('GET', "/en/add_favorites/{$slug}/{$user->getId()}");
+        $countAfter = count($user->getEstates());
+        //$this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertCount(
+
+        );
+    }
 }
