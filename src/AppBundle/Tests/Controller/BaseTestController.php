fc42989d434f71c235e4838bd32f169101dc5e0e
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-04-07 15:32:56 +0300

    ok

--- src/AppBundle/Tests/Controller/BaseTestController.php
+++ src/AppBundle/Tests/Controller/BaseTestController.php
@@ -1,18 +1,25 @@
 <?php
 namespace AppBundle\Tests\Controller;
 
-use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class BaseTestController extends WebTestCase
 {
+    /** @var Client */
     protected $client = null;
 
     public function setUp()
     {
         $this->client = static::createClient();
+        $this->runCommand(['command' => 'doctrine:database:create']);
+        $this->runCommand(['command' => 'doctrine:schema:update', '--force' => true]);
+        $this->runCommand(['command' => 'doctrine:fixtures:load', '--no-interaction' => true]);
     }
 
     protected function logIn($role)
@@ -29,4 +36,21 @@ class BaseTestController extends WebTestCase
         $cookie = new Cookie($session->getName(), $session->getId());
         $this->client->getCookieJar()->set($cookie);
     }
+
+    public function tearDown()
+    {
+        $this->runCommand(['command' => 'doctrine:database:drop', '--force' => true]);
+        $this->client = null;
+    }
+
+    protected function runCommand( array $arguments = [] )
+    {
+        $application = new Application($this->client->getKernel());
+        $application->setAutoExit(false);
+        $arguments['--quiet'] = true;
+        $arguments['-e'] = 'test';
+        $input = new ArrayInput($arguments);
+        $application->run($input, new ConsoleOutput());
+    }
 }
+
