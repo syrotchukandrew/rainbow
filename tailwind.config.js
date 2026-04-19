1d8a2145a03234b060169a73f2251eb5cf22d3ec
Andrew Syrotchuk <andrewsyrotchuk@gmail.com>
2026-04-19 21:07:18 +0200

    Added Georgia serif font family to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,9 @@ module.exports = {
                 md: 'calc(var(--radius) - 2px)',
                 sm: 'calc(var(--radius) - 4px)',
             },
+            fontFamily: {
+                serif: ['Georgia', '"Times New Roman"', 'serif'],
+            },
         },
     },
     plugins: [],
